24a681e74e3f90713c9a12ba8b5fdac8d73ddb30
Faiyaaz <faiyaaz.haque90@gmail.com>
2022-09-17 22:17:27 -0400

    added bootstrap for styling; setup navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import {LinkContainer} from "react-router-bootstrap"
 
 function App() {
-  return (
-    <div >
-      <ul>
-        <Link to="/">
-        <li>Home</li>
-        </Link>
-        <Link to="/About">
-        <li>About</li>
-        </Link>
-        <Link to="/Shop">
-        <li>Shop</li>
-        </Link>
-      </ul>
-      <h2>Welcome to the shop-zone</h2>
-    </div>
-  );
+    return (
+      <>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand >The Shop</Navbar.Brand>
+            <Nav className="me-auto">
+              <LinkContainer to="/">
+                <Nav.Link >Home</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/About">
+                <Nav.Link >About</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/Shop">
+                <Nav.Link >Shop</Nav.Link>
+              </LinkContainer>
+            </Nav>        
+          </Container>
+        </Navbar>
+      </>
+    );
 }
 
-export default App;
+export default App
