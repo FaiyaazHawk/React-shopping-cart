1a621421852c449734cc80821b5f236627105038
Faiyaaz <faiyaaz.haque90@gmail.com>
2022-09-21 17:29:38 -0400

    got add and remove button hooked to state

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,14 +5,30 @@ import { Container, Button } from "react-bootstrap";
 
 
 const Card = (props) => {
-    console.log(props)
+    const [quantity, setQuantity] = React.useState(0)
+
+
+
+    const incrementAmount = () => {
+        setQuantity(prevQuantity => prevQuantity+1)
+    }
+
+    const decrementAmount = () => {
+        setQuantity(prevQuantity => prevQuantity-1)
+    }
+
+    
+
+    
     return (
         <>
             <Container className="container">
                 <img className="img-thumbnail" src={props.item.image} alt={props.item.title}/>
                 <h3>Price:{props.item.price}</h3>
-                <Button>Add</Button>
-                <Button>Remove</Button>
+                <input type="number" id="number" value={quantity}></input>
+                <Button onClick={incrementAmount}>Add</Button>
+                <Button onClick={decrementAmount}>Remove</Button>
+                <button >Add to cart</button>
                 <h1>{props.item.title}</h1>
                 <p>{props.item.description}</p>
             </Container>
