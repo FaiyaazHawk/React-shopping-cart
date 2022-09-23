32230abaa38576b1646ec60614b4eb9cb9557a22
Faiyaaz <faiyaaz.haque90@gmail.com>
2022-09-23 03:29:16 -0400

    go build out the checkout page

--- src/components/CheckoutCard.js
+++ src/components/CheckoutCard.js
@@ -6,6 +6,7 @@ const CheckoutCard = (props) => {
     return (
         <div>
             <h1>Count: {props.item.count}</h1>
+            <h1>Name: {props.item.item.title}</h1>
         </div>
     )
 
